3092038a8e61592fccf8e8bcffda8d11999ac391
Julio Alorro <alorro3@gmail.com>
2020-04-14 22:54:36 +0200

    Added missing header

--- Sources/CodeBuilder/Core/Code.swift
+++ Sources/CodeBuilder/Core/Code.swift
@@ -1,7 +1,8 @@
 //
-//  File.swift
-//  
+//  Code.swift
 //
+//  Copyright (c) Julio Miguel Alorro 2020
+//  MIT license, see LICENSE file for details
 //  Created by Julio Miguel Alorro on 07.04.20.
 //
 
