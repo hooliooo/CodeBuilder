0b5cfbb81e69927d4bbc7e45fe0687db6d2dc9a5
Julio Alorro <alorro3@gmail.com>
2020-04-28 18:32:49 +0200

    Added ForEach struct

---
+++ Sources/CodeBuilder/Core/ForEach.swift
@@ -0,0 +1,55 @@
+//
+//  ForEach.swift
+//
+//  Copyright (c) Julio Miguel Alorro 2020
+//  MIT license, see LICENSE file for details
+//  Created by Julio Miguel Alorro on 4/28/20.
+//
+
+import Foundation
+
+/**
+ A struct that enables interation through a collection.
+ */
+public struct ForEach<T: RandomAccessCollection>: CodeRepresentable {
+
+    // MARK: Initializers
+    /**
+     Initializer
+     - parameters:
+        - elements: The collection of elements
+        - builder: The CodeBuilder closure executed on each element of the collection
+     */
+    public init(_ elements: T, @CodeBuilder _ builder: @escaping (T.Element) -> CodeRepresentable) {
+        self.elements = elements
+        self.builder = builder
+    }
+
+    // MARK: Stored Properties
+    /**
+     A collection of elements to be iterated on
+     */
+    public let elements: T
+
+    /**
+     The closure executed on each element that produces a CodeRepresentable
+     */
+    @inlinable
+    public let builder: (T.Element) -> CodeRepresentable
+
+    /**
+     The CodeRepresentables created by the builder are joined together with line breaks in between
+     */
+    @inlinable
+    public var asCode: Code {
+        let representables: [CodeRepresentable] = self.elements.map { (element) -> CodeRepresentable in
+            self.builder(element)
+        }
+
+        let fragments: [Fragment] = zip(representables, representables.dropFirst())
+            .flatMap { (first, second) -> [Fragment] in
+                first.asCode.fragments + [lineBreak()] + second.asCode.fragments
+            }
+        return Code.fragments(fragments)
+    }
+}
--- Tests/Integration/FreeFunctionTests.swift
+++ Tests/Integration/FreeFunctionTests.swift
@@ -13,7 +13,18 @@ final class FreeFunctionTests: XCTestCase {
                                   - y: Second Int
                                - returns: The sum of the two Ints
                                */
-                              public func sum(x: Int, y: Int) -> Int {
+                              public func this(x: Int, y: Int) -> Int {
+                                  return x + y
+                              }
+
+                              /**
+                               Sums up two integers
+                               - parameters:
+                                  - x: First Int
+                                  - y: Second Int
+                               - returns: The sum of the two Ints
+                               */
+                              public func that(x: Int, y: Int) -> Int {
                                   return x + y
                               }
 
@@ -21,18 +32,23 @@ final class FreeFunctionTests: XCTestCase {
         let arg1 = Argument(name: "x", type: "Int")
         let arg2 = Argument(name: "y", type: "Int")
         let actual: String = fileSpec(fileName: "", indent: "    ") {
-            documentationSpec(
-                "Sums up two integers",
-                format: Documentation.Format.multiline,
-                parameters: [
-                    arg1.asParameter(documentation: "First Int"),
-                    arg2.asParameter(documentation: "Second Int")
-                ], returns: "The sum of the two Ints"
-            )
-            functionSpec("sum", access: .public, arguments: [arg1, arg2], returnValue: "Int") {
-                statement("return x + y")
+
+            ForEach(["this", "that"]) { element in
+                documentationSpec(
+                    "Sums up two integers",
+                    format: Documentation.Format.multiline,
+                    parameters: [
+                        arg1.asParameter(documentation: "First Int"),
+                        arg2.asParameter(documentation: "Second Int")
+                    ], returns: "The sum of the two Ints"
+                )
+                functionSpec(element, access: .public, arguments: [arg1, arg2], returnValue: "Int") {
+                    statement("return x + y")
+                }
             }
+
         }.string
+        print(actual)
         XCTAssert(expected == actual, "The two strings should equal each other")
 
     }
